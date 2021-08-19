c4e2051a611e1db9ae4a2a8d74128834c2b9d468
Olomije Ezekiel <olomijeezekiel@gmail.com>
2021-08-19 21:19:03 +0100

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2d458720311695b43dd3badc9277f45de7f03beb',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2d458720311695b43dd3badc9277f45de7f03beb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
