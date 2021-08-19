e3552be20c07f798da40d0d5170a1505a353a83a
hashsage <71348941+hashsage@users.noreply.github.com>
2021-08-20 00:24:08 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,21 +1,43 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
 
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
 
 if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
-	$name=$_POST['name'];
-	$subject=$_POST['subject'];
-	$mailFrom=$_POST['email'];
-	$message=$_POST['message'];
-	
-$mailTo = "olomijeezekiel@yahoo.com"
-$headers = "From: ".$mailFrom 
-$txt = "";
-	$txt .="You have received an email from " .$name. "\r\n";
-	$txt .= .$message. "\r\n";
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'lamudugbe@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'iamsage26+'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
 
+    $mail->setFrom('lamudugbe@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('lamudugbe@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
-mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
 
+    $mail->send();
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
 }
+?>
