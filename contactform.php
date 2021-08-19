fa751dd4628859e09d26eb903ffbc8a4a2651920
hashsage <71348941+hashsage@users.noreply.github.com>
2021-08-20 00:24:53 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once 'phpmailer/Exception.php';
-require_once 'phpmailer/PHPMailer.php';
-require_once 'phpmailer/SMTP.php';
+require_once 'Exception.php';
+require_once 'PHPMailer.php';
+require_once 'SMTP.php';
 
 $mail = new PHPMailer(true);
 
