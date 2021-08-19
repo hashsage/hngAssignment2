983e52dbcea66d08500b150da02c8a00b9f96e9a
hashsage <71348941+hashsage@users.noreply.github.com>
2021-08-19 23:11:28 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -4,6 +4,7 @@
 if(isset($_POST['submit'])){
 
 	$name=$_POST['name'];
+	$subject=$_POST['subject'];
 	$mailFrom=$_POST['email'];
 	$message=$_POST['message'];
 	
@@ -12,7 +13,7 @@ $headers = "From: ".$mailFrom
 $txt = "You have received an email from ".$name.".\n\n".$message;
 
 
-mail($mailTo, $txt, $headers);
+mail($mailTo, $subject, $txt, $headers);
 header("Location: index.php?mailsend");
 
 }
