857da41b20ba89fa64ef54c5a86e02e1f5ff4596
hashsage <71348941+hashsage@users.noreply.github.com>
2021-08-19 23:22:35 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
 	
 $mailTo = "olomijeezekiel@gmail.com"
 $headers = "From: ".$mailFrom 
-$txt = "You have received an email from ".$name.".\n\n".$message;
+$txt = "You have received an email from " .$name. "\n\n" .$message;
 
 
 mail($mailTo, $subject, $txt, $headers);
